95ac64df653f0bcc9e105cd0abfc6260e222b9ea
kawa-0x0-uso <96374232+kawa-0x0-uso@users.noreply.github.com>
2022-04-24 22:45:56 +0900

    ソースの不要部分削除

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 // APIキー
 const API_KEY = "5ad1350dbe77f2d0fd0cb99858d989e5";
-// 表示領域のidを取得
-// const weather_Area = document.getElementById("weather");
 // 都市名を格納する変数
 let city ="";
 
@@ -21,6 +19,7 @@ async function callApi(city){
     const res = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=ja&APPID=" + API_KEY);
     const weather = await res.json();
 
+    // 取得したい情報と値を配列に格納
     const weather_Info ={
         weatherCity : city,
         weatherMain : weather.weather[0].main,
@@ -31,40 +30,9 @@ async function callApi(city){
         weatherFeels_like : weather.main.feels_like
     };
 
+    // 配列から値を取り出し画面に出力
     for (let key in weather_Info) {
-        console.log('key:' + key + ' value:' + weather_Info[key]);
         let weatheDate = document.getElementById(key);
         weatheDate.innerText = weather_Info[key];
     }
-
-    console.log("配列てすと："+weather_Info.weatherMain);
-
-    // // 都市名
-    // const weatherCity = document.createElement("li");
-    // weatherCity.innerText = weather.name;
-    // weather_Area.appendChild(weatherCity);
-    // // 天気
-    // const weatherMain = document.createElement("li");
-    // weatherMain.innerText = weather.weather[0].main;
-    // weather_Area.appendChild(weatherMain);
-    // // 天気詳細
-    // const weatherDescription = document.createElement("li");
-    // weatherDescription.innerText = weather.weather[0].description;
-    // weather_Area.appendChild(weatherDescription);
-    // // 気温
-    // const weatherTemp = document.createElement("li");
-    // weatherTemp.innerText = weather.main.temp;
-    // weather_Area.appendChild(weatherTemp); 
-    // // 最高気温
-    // const weatherTemp_max = document.createElement("li");
-    // weatherTemp_max.innerText = weather.main.temp_max;
-    // weather_Area.appendChild(weatherTemp_max);
-    // // 最低気温
-    // const weatherTemp_min = document.createElement("li");
-    // weatherTemp_min.innerText = weather.main.temp_min;
-    // weather_Area.appendChild(weatherTemp_min); 
-    // // 体感温度
-    // const weatherFeels_like = document.createElement("li");
-    // weatherFeels_like.innerText = weather.main.feels_like;
-    // weather_Area.appendChild(weatherFeels_like); 
 };
